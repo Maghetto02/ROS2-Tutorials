3f963f495dc438d1677b9173c5f43b3d2713f04d
Maghetto <onofrio.1559@gmail.com>
2023-10-25 22:48:36 +0200

    added rosbag tutorial files

---
+++ ros2_ws/build/bag_recorder_nodes/CMakeFiles/data_generator_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_generator_node.dir/src/data_generator_node.cpp.o"
+  "data_generator_node"
+  "data_generator_node.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_generator_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
